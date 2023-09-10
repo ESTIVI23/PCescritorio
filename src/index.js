17518f01b64aca5227376517e77f0fb6eed7a354
ramirezvictorh23@gmail.com <ramirezvictorh23@gmail.com>
2023-09-10 18:53:18 -0300

    tercerprueba de comi 1

---
+++ src/index.js
@@ -0,0 +1,16 @@
+var variableVar = 10;
+let numero =50; 
+
+if(numero > 40){
+    var variableVar =20;
+    console.log(variableVar);
+
+    let numero=80;
+    console.log("este numero es dentro del if" +numero);
+}
+
+let numerow=22;
+
+
+console.log("fuera del if " + variableVar);
+console.log("tambien fuera del ir " + numero);
